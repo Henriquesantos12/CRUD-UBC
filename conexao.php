0b28ff0f50e67869ce8c1ded3a3a6791cd9c0187
Henrique Santos <114838533+Henriquesantos12@users.noreply.github.com>
2024-10-01 17:14:58 -0300

    Commitando meu primeiro commit

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pass = "root";
+    $bd = "empresa";
+   if( $conn =  mysqli_connect($server ,$user, $pass, $bd)){
+   // echo "conectado";
+   }else{
+    echo "Erro";
+   }
+
+   function mensagem($texto, $tipo) {
+    echo "<div class='alert alert-$tipo role='alert'>
+             $texto;
+        </div>";
+   }
+?>
